054b82328b6271882c3da3e9d4809fac19e02782
Daffa Abdul Fatah <muhammaddaffaabdulfatah@Alchemyxzxs-MacBook-Air.local>
2022-03-22 02:39:56 +0700

    latihan

---
+++ kuliah/pertemuan5/latihan3_kuliah.php
@@ -0,0 +1,17 @@
+<?php
+$mahasiswa = [
+["Daffa Abdul Fatah", "213040038", "daffaabdulfatah@gmail.com", "Teknik Informatika"],
+["Paisal Ambari", "213040065", "paisalambari@gmail.com", "Teknik Informatika"],
+["Muhammad Jauhari", "213040060", "jauhari@gmail.com", "Teknik Informatika"],
+["Juan Aditya", "213040053", "juanaditya@gmail.com", "Teknik Informatika"],
+]
+?>
+
+<?php foreach($mahasiswa as $mhs)  { ?>
+<ul>
+    <li>Nama: <?php echo $mhs[0] ?></li>
+    <li>NPM: <?php echo $mhs[1] ?></li>
+    <li>Email: <?php echo $mhs[2] ?></li>
+    <li>Jurusan: <?php echo $mhs[3]?></li>
+</ul>
+<?php } ?>
